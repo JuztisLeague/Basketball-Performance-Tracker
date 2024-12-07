ed3b9078a1c68be80b42446b24248d45a2e6c75f
mis2gan <167780395+mis2gan@users.noreply.github.com>
2024-12-07 21:54:02 +0800

    Add files via upload

--- basketballpt.sql
+++ basketballpt.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 07, 2024 at 02:42 PM
+-- Generation Time: Dec 07, 2024 at 02:53 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
